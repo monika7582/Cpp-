3a683f2a0a6c157c6ae219cd307f30d5b5283a7b
monika7582 <monika.sahu2023@ssipmt.com>
2025-02-13 17:13:39 +0530

    Add files via upload

---
+++ Pattern1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout << "Enter value of x: ";
+    cin >> x;
+    for(int i=0; i<=x; i++)
+    {
+        cout<< "*" <<" " << endl;
+    }
+    return 0;
+}
